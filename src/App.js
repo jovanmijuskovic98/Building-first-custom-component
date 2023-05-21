6807d8677724c2d1a85122ccfb27e6983c6033e8
Jovan <jovanmijuskovic11@gmail.com>
2023-05-21 15:45:00 +0200

    #

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
+// const App = () => {} Alternative Arrow function syntax
 function App() {
 
   const expenses = [
